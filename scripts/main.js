22b27a0b3318bf80cd1d25322de74f9100513adb
phfryed <84614846+phfryed@users.noreply.github.com>
2021-05-30 14:23:04 +0800

    fix image switch?

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,8 @@
-//const myHeading = document.querySelector('h1');
-//myHeading.textContent = 'hello hello';
-
-document.querySelector('html').onclick = function() {
-	alert('Ouch! Stop poking me!');
-}
-
-/*
 let myImage = document.querySelector('img');
 
 myImage.querySelector('img').onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/potato.png') {
-    	alert('oi');
       myImage.setAttribute('src','images/avocado.png');
     } else {
       myImage.setAttribute('src','images/potato.png');
