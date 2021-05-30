734cb3fbfb9de2b2138afc985a8a6a22efba28b0
phfryed <84614846+phfryed@users.noreply.github.com>
2021-05-30 14:50:19 +0800

    add personalised name prompt

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,37 @@
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-    
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/potato.png') {
       myImage.setAttribute('src','images/avocado.png');
     } else {
       myImage.setAttribute('src','images/potato.png');
     } 
+}
+
+// Personalized welcome message code
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+  let myName = prompt('please enter your name.');
+  if(!myName){
+    setUserName();
+  } else {
+  localStorage.setItem('name',myName);
+  myHeading.textContent = 'hi ' + myName;
+  }
+}
+
+if  (!localStorage.getItem('name')){
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'hi ' + storedName;
+}
+
+myButton.onclick = function(){
+  setUserName();
 }
