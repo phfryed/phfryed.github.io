185cfe93ecaa499449f053b6e83659c42af1b93f
phfryed <84614846+phfryed@users.noreply.github.com>
2021-05-30 14:33:04 +0800

    fix image switch for real

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
+/*document.querySelector('img').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}*/
+
 let myImage = document.querySelector('img');
 
-myImage.querySelector('img').onclick = function() {
+myImage.onclick = function() {
+    
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/potato.png') {
       myImage.setAttribute('src','images/avocado.png');
     } else {
       myImage.setAttribute('src','images/potato.png');
-    }
+    } 
 }
