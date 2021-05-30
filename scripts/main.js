84ddb73425edf7809d6586ef5aa5cd1cade42581
phfryed <84614846+phfryed@users.noreply.github.com>
2021-05-30 14:20:43 +0800

    add image switch

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+//const myHeading = document.querySelector('h1');
+//myHeading.textContent = 'hello hello';
+
+document.querySelector('html').onclick = function() {
+	alert('Ouch! Stop poking me!');
+}
+
+/*
+let myImage = document.querySelector('img');
+
+myImage.querySelector('img').onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/potato.png') {
+    	alert('oi');
+      myImage.setAttribute('src','images/avocado.png');
+    } else {
+      myImage.setAttribute('src','images/potato.png');
+    }
+}
